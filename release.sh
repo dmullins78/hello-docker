41796568bba7bc609b5ab4d307bb027ee2374b45
Dan Mullins <dmullins78@gmail.com>
2016-01-31 17:26:56 -0600

    Releasing...

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-mvn package \
+mvn verify \
   build-helper:parse-version \
   scm:tag -Dtag=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion} \
   versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} \
