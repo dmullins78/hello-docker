578147d73235011f2746f142a56aa169f24f98e6
Dan Mullins <dmullins78@gmail.com>
2016-02-01 23:08:39 -0600

    Releasing...

--- release.sh
+++ release.sh
@@ -4,6 +4,6 @@
   mvn build-helper:parse-version \
   versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} \
   versions:commit \
-  scm:checkin -Dbasedir='./' -Dmessage='Releasing...'  -X
+  scm:checkin -Dbasedir='./' -Dmessage='Releasing...' 
 
 
