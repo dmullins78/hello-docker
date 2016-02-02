1b894e392b5fd806b235c05f1889d7309b2ff9f4
Dan Mullins <dmullins78@gmail.com>
2016-02-01 23:03:40 -0600

    Releasing...

--- release.sh
+++ release.sh
@@ -2,7 +2,8 @@
   #verify \
   #scm:tag -Dtag=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion} \
   mvn build-helper:parse-version \
-  versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion} \
+  versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} \
+  versions:commit \
   scm:checkin -Dbasedir='./' -Dmessage='Releasing...' \
 
 
