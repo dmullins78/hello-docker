606d53ec1cdd17898b646542ba196aa4a364b212
Dan Mullins <dmullins78@gmail.com>
2016-02-03 06:47:35 -0600

    Releasing...

--- src/main/groovy/com/dan/test/HelloServlet.groovy
+++ src/main/groovy/com/dan/test/HelloServlet.groovy
@@ -12,6 +12,11 @@ public class HelloServlet extends HttpServlet {
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     String mode = System.getenv("HELLO_MODE")
 
+    Map<String, String> env = System.getenv();
+    for (String envName : env.keySet()) {
+      System.out.format("%s=%s%n", envName, env.get(envName));
+    }
+
     PrintWriter out = response.getWriter()
     out.println("Hello from  " + mode)
     out.close()
