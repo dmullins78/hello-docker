97d56b17a9af7e10c06d71a557b3f9aecbc97b9a
Dan Mullins <dmullins78@gmail.com>
2016-02-06 13:34:47 -0600

    renaming since I do not use spark

---
+++ hello-docker/release.sh
@@ -0,0 +1,10 @@
+mvn clean \
+  verify \
+  build-helper:parse-version \
+  scm:tag -Dtag=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion} \
+  versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} \
+  versions:commit \
+  scm:checkin -Dbasedir='./' -Dmessage='Releasing...' \
+  docker:build -DpushImageTag 
+
+
